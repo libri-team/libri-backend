3bc3d7e2ec042164da684d5bf8379dd6c373906b
minhan <minhan@toss.im>
2025-02-12 22:12:36 +0900

    deploy

--- docker/config/local/1_create_users.sql
+++ docker/config/local/1_create_users.sql
@@ -1,11 +1,11 @@
 USE mysql;
 
 -- 전체 권한을 가진 사용자 생성
-CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+CREATE USER 'libri_user'@'%' IDENTIFIED BY 'libri_local_pass';
+GRANT ALL PRIVILEGES ON libri_db.* TO 'libri_user'@'%';
 
 -- READ 전용 사용자 생성
-CREATE USER '${MYSQL_READ_USER}'@'%' IDENTIFIED BY '${MYSQL_READ_PASSWORD}';
-GRANT SELECT ON ${MYSQL_DATABASE}.* TO '${MYSQL_READ_USER}'@'%';
+CREATE USER 'libri_reader'@'%' IDENTIFIED BY 'reader_local_pass';
+GRANT SELECT ON libri_db.* TO 'libri_reader'@'%';
 
 FLUSH PRIVILEGES;
